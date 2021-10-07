c24f3b8959287acd6d7bc0fcbd39b31d75afa37c
Didem Tumer <didemdizman@gmail.com>
2021-10-07 09:27:59 +0300

    Adding img to section-how part

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Header/>
+    <Header />
     <section class="section-hero">
       <div class="hero">
         <div class="hero-text-box">
@@ -12,18 +12,40 @@
             healthy again. Tailored to your personal tastes and nutritional
             needs.
           </p>
-          <a href="#" class="btn btn--full margin-right-small">Start eating well</a>
+          <a href="#" class="btn btn--full margin-right-small"
+            >Start eating well</a
+          >
           <a href="#" class="btn btn--outline">Learn more &darr;</a>
           <div class="delivered-meals">
             <div class="delivered-imgs">
-              <img src="./assets/img/customers/customer-1.jpg" alt="Customer Photo">
-              <img src="./assets/img/customers/customer-2.jpg" alt="Customer Photo">
-              <img src="./assets/img/customers/customer-3.jpg" alt="Customer Photo">
-              <img src="./assets/img/customers/customer-4.jpg" alt="Customer Photo">
-              <img src="./assets/img/customers/customer-5.jpg" alt="Customer Photo">
-              <img src="./assets/img/customers/customer-6.jpg" alt="Customer Photo">
+              <img
+                src="./assets/img/customers/customer-1.jpg"
+                alt="Customer Photo"
+              />
+              <img
+                src="./assets/img/customers/customer-2.jpg"
+                alt="Customer Photo"
+              />
+              <img
+                src="./assets/img/customers/customer-3.jpg"
+                alt="Customer Photo"
+              />
+              <img
+                src="./assets/img/customers/customer-4.jpg"
+                alt="Customer Photo"
+              />
+              <img
+                src="./assets/img/customers/customer-5.jpg"
+                alt="Customer Photo"
+              />
+              <img
+                src="./assets/img/customers/customer-6.jpg"
+                alt="Customer Photo"
+              />
             </div>
-            <p class="delivered-text"><span>250,000+</span> meals delivered last year!</p>
+            <p class="delivered-text">
+              <span>250,000+</span> meals delivered last year!
+            </p>
           </div>
         </div>
         <div class="hero-image-box">
@@ -35,28 +57,28 @@
         </div>
       </div>
     </section>
-    <section class="section-how">
-      <div class="container grid grid--2-cols">
-        <div>Test 1</div>
-        <div>Test 2</div>
-        <div>Test 3</div>
-        <div>Test 4</div>
-      </div>
-    </section>
+    <SectionHow />
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
+import Header from "./components/Header.vue";
+import SectionHow from "./components/SectionHow.vue";
 export default {
-  components: { Header },
+  components: {
+    Header,
+    SectionHow,
+  },
+  methods: {
+  
+  },
 };
 </script>
 
 <style>
-.section-hero{
-background-color: #fdf2e9;
-padding: 4.8rem 0 9.6rem 0 ;
+.section-hero {
+  background-color: #fdf2e9;
+  padding: 4.8rem 0 9.6rem 0;
 }
 .hero {
   max-width: 130rem;
@@ -67,78 +89,91 @@ padding: 4.8rem 0 9.6rem 0 ;
   align-items: center;
   gap: 9.6rem;
 }
-.heading-primary{
-  font-size: 5.2rem;
+.heading-primary,
+.heading-secondary {
   font-weight: 700;
-  line-height: 1.05;
-  color: #333;
   letter-spacing: -0.5px;
+  color: #333;
+  /* color: #45260a; */
+}
+.heading-primary {
+  font-size: 5.2rem;
+  line-height: 1.05;
   margin-bottom: 3.2rem;
 }
-.hero-description{
+.heading-secondary {
+  font-size: 4.4rem;
+  line-height: 1.2;
+  margin-bottom: 9.6rem;
+}
+.heading-tertiary {
+  font-size: 3.6rem;
+  line-height: 1.2;
+  margin-bottom: 3.2rem;
+}
+.subheading {
+  display: block;
+  font-size: 1.6rem;
+  font-weight: 500;
+  color: #cf711f;
+  text-transform: uppercase;
+  margin-bottom: 1.6rem;
+  letter-spacing: 0.75px;
+}
+.hero-description {
   font-size: 2rem;
   line-height: 1.5;
   margin-bottom: 4.8rem;
 }
-.hero-img{
+.hero-img {
   width: 100%;
 }
-.margin-right-small{
+.margin-right-small {
   margin-right: 1.6rem !important;
 }
-.delivered-meals{
+.delivered-meals {
   display: flex;
   margin-top: 8rem;
   align-items: center;
   gap: 1.6rem;
 }
-.delivered-imgs{
+.delivered-imgs {
   display: flex;
 }
-.delivered-imgs img{
+.delivered-imgs img {
   height: 4.8rem;
   width: 4.8rem;
   border-radius: 50%;
   margin-right: -1.6rem;
   border: 3px solid #fdf2e9;
 }
-.delivered-imgs img:last-child{
+.delivered-imgs img:last-child {
   margin: 0;
 }
-.delivered-text{
+.delivered-text {
   font-size: 1.8rem;
   font-weight: 600;
 }
-.delivered-text span{
+.delivered-text span {
   color: #cf711f;
   font-weight: 700;
 }
-.section-how{
-  padding: 9.6rem 0;
-  background-color: orangered;
-}
-.container{
+.container {
   max-width: 120rem;
   margin: 0 auto;
   padding: 0 3.2rem;
 }
-.grid{
+.grid {
   display: grid;
   gap: 9.6rem;
-  
 }
-.grid--2-cols{
+.grid--2-cols {
   grid-template-columns: repeat(2, 1fr);
 }
-.grid--3-cols{
+.grid--3-cols {
   grid-template-columns: repeat(3, 1fr);
 }
-.grid--4-cols{
+.grid--4-cols {
   grid-template-columns: repeat(4, 1fr);
-}
-.section-how div div{
-  padding: 100px;
-  font-size: 50px;
-  background-color: #333;
 }
 </style>
